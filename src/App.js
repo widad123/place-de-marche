377960275329ffe1d9d8e6f5949e0e4fb6a2b993
widad123 <khatiri.widad@gmail.com>
2020-03-04 16:32:28 +0100

    problm chargement contract

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useState, useEffect}from 'react';
-import PlaceDeMarche from './contracts/placeDeMarche.json';
+import placeDeMarche from './contracts/placeDeMarche.json';
 import getWeb3 from './getWeb3';
 import './App.css';
 
@@ -7,7 +7,7 @@ function App (){
   const [balance,setbalance ]=useState(undefined);
   const [web3,setweb3]=useState(undefined);
   const [accounts,setAccounts]=useState([]);
-  const [contract,setContract]=useState([]);
+  const [marche,setmarche]=useState([]);
 
   useEffect(()=>{
     const init = async()=>{
@@ -15,18 +15,20 @@ function App (){
         const web3=await getWeb3();
         const accounts=await web3.eth.getAccounts();
         const networkId= await web3.eth.net.getId();
-        const deployedNetwork=PlaceDeMarche.networks[networkId];
-        const contract = new web3.eth.Contract(
-          PlaceDeMarche.abi,
-          deployedNetwork && deployedNetwork.address,
-        );
-
+        const networkData=placeDeMarche.networks[networkId];
+        if (networkData) {
+          const marche = new web3.eth.Contract(
+            placeDeMarche.abi,
+            networkData && networkData.address,
+          );
+          
         setweb3(web3);
         setAccounts(accounts);
-        setContract(contract);
+        setmarche(marche);
+        }
       } catch (error) {
         alert(
-          'failed to load web3, accounts, or contracts, Check console for details',
+          'failed to load web3, accounts, or marches, Check console for details',
         );
         console.error(error);
       }
@@ -36,18 +38,18 @@ function App (){
 
   useEffect(()=>{
     const load = async()=>{
-      const balance= await contract.methods.balanceOf().call();
-      setbalance(balance);
+      const response= await marche.methods.balanceOf().call();
+      setbalance(response);
     }
     if(typeof web3!=='undefined'
     && typeof accounts!=='undefined'
-    && typeof contract!=='undefined'){
+    && typeof marche!=='undefined'){
       load();
     }
-  },[web3, accounts, contract]);
+  },[web3, accounts, marche]);
 
-  if(typeof web3!=='undefined'){
-    return <div>Loading web3, accounts, and contracts ...</div>;
+  if(typeof web3 ==='undefined'){
+    return <div>Loading web3, accounts, and marches ...</div>;
   }
 
   return (
